78ad579f03cbd55e0073ecb0e7655ae6586c123f
Neda <nedachangizi@gmail.com>
2022-11-16 13:16:18 -0800

    function declaration practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
-// function greet(user) {
+// function greet( time = "day", user) {
 // //   console.log("Hello " + user);
-//   console.log(`Hello ${user}`);
+//   console.log(`Good ${time} ${user}`);
 // }
+
+// greet(undefined,"Neda");
+
+// greet("Ian");
 
 // const greet = function (user) {
 //   //   console.log("Hello " + user);
 //   console.log(`Hello ${user}`);
 // }
 
-const greet =  (user) => {
-    //   console.log("Hello " + user);
-    console.log(`Hello ${user}`);
-  }
+// const greet =  (user) => {
+//     //   console.log("Hello " + user);
+//     console.log(`Hello ${user}`);
+//   }
 
-greet("Neda");
-greet("Ian");
+
