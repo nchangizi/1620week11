9dba4a4c6666ffbc4596cccdf192f892cb1b7eec
Neda <nedachangizi@gmail.com>
2022-11-16 13:38:36 -0800

    update p

--- scripts/script.js
+++ scripts/script.js
@@ -24,5 +24,9 @@ function calcArea(radius) {
 
 let r = prompt("Give a readius");
 let calculatedArea = calcArea(r).toFixed(2);
- 
 alert(`The area of a circle with radius ${r} is ${calculatedArea}`);
+
+let radiusParagraph = document.querySelector("#radius");
+radiusParagraph.textContent += r;
+let areaParagraph = document.querySelector("#result");
+areaParagraph.textContent += calculatedArea
